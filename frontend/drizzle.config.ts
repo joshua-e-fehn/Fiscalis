c3edbc2d351a8bf48c4d4d5ef3e8780506aa8c31
Joshua Fehn <joshua.fehn@tum.de>
2024-07-02 16:12:41 +0200

    Add DrizzleORM with Supabase Postgres DB

---
+++ frontend/drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "drizzle-kit";
+import { config } from "dotenv";
+
+config({ path: ".env.local" });
+
+export default defineConfig({
+  schema: "./db/drizzle/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+  verbose: true,
+  strict: true,
+});

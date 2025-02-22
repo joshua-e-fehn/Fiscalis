bc8b7f5cbe31c3d52fe9feb33329a7f92fdd9418
Joshua Fehn <joshua.fehn@tum.de>
2025-02-22 17:48:49 +0100

    Create custom financial KPI card
    So far the data can not be passed down from the parent component.

---
+++ frontend/components/atomic/atoms/cards/singleKPICard.tsx
@@ -0,0 +1,24 @@
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardDescription,
+} from "@/components/ui/shadcn/card";
+
+import { Euro } from "lucide-react";
+
+export function SingleKPICard() {
+  return (
+    <Card x-chunk="dashboard-01-chunk-0">
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
+        <Euro className="h-4 w-4 text-muted-foreground" />
+      </CardHeader>
+      <CardContent>
+        <div className="text-2xl font-bold">€4,231.89</div>
+        <p className="text-xs text-muted-foreground">+20.1% from last month</p>
+      </CardContent>
+    </Card>
+  );
+}

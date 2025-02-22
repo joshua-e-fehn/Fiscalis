cd3f98399dc2d30d945e8de3033d956e58e0abe0
Joshua Fehn <joshua.fehn@tum.de>
2025-02-22 17:46:51 +0100

    Add custom Donut Chart with total quantity
    This component is heavily based on the Shadcn UI pie charts.
    Shadcn UI charts are based in Recharts.
    This chart is a donut chart with a text in the middle, representing the total of all quantities.
    It is available in 4 different sizes: "sm", "md", "lg" and "xl".
    Additionally you can set custom size properties.
    It can be used in a financial related context (adding the currency symbol to the total) or everywhere else (no currency symbol).
    It has a Title and a Subtitle. As well as a subtitle for the total value.
    At the bottom you can add a description consisting of a title and subtitle.
    The data and the data format can be passed down to the component from the parent component.

---
+++ frontend/components/atomic/atoms/pieCharts/donutWithText.tsx
@@ -0,0 +1,203 @@
+"use client";
+
+import * as React from "react";
+import { TrendingUp } from "lucide-react";
+import { Label, Pie, PieChart } from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/shadcn/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/shadcn/chart";
+
+interface PieChartDonutWithTextProps {
+  chartData: any[] | undefined;
+  chartConfig: ChartConfig;
+  dataKey: string;
+  nameKey: string;
+  title: string;
+  description: string;
+  subTitle?: string;
+  subDescription?: string;
+  isFinancial?: boolean;
+  currency?: string;
+  size?: "sm" | "md" | "lg" | "xl";
+  customSize?: {
+    width: number;
+    height: number;
+    innerRadius: number;
+    fontSizeTitle: string;
+    fontSizeDescription: string;
+    fontSizeSubTitle: string;
+    fontSizeSubDescription: string;
+    fontSizeTotalTitle: string;
+    fontSizeTotalDescription: string;
+    totalQuantityPadding: number;
+    cardWidth: string;
+    strokeWidth: number;
+  };
+}
+
+const CHART_SIZES = {
+  sm: {
+    width: 300,
+    height: 400,
+    innerRadius: 60,
+    fontSizeTitle: "text-2xl",
+    fontSizeDescription: "text-base",
+    fontSizeSubTitle: "text-base",
+    fontSizeSubDescription: "text-sm",
+    fontSizeTotalTitle: "text-xl",
+    fontSizeTotalDescription: "text-lg",
+    totalQuantityPadding: 24,
+    strokeWidth: 5,
+  },
+  md: {
+    width: 400,
+    height: 500,
+    innerRadius: 90,
+    fontSizeTitle: "text-4xl",
+    fontSizeDescription: "text-lg",
+    fontSizeSubTitle: "text-lg",
+    fontSizeSubDescription: "text-base",
+    fontSizeTotalTitle: "text-3xl",
+    fontSizeTotalDescription: "text-xl",
+    totalQuantityPadding: 30,
+    strokeWidth: 5,
+  },
+  lg: {
+    width: 500,
+    height: 600,
+    innerRadius: 120,
+    fontSizeTitle: "text-5xl",
+    fontSizeDescription: "text-xl",
+    fontSizeSubTitle: "text-xl",
+    fontSizeSubDescription: "text-lg",
+    fontSizeTotalTitle: "text-4xl",
+    fontSizeTotalDescription: "text-2xl",
+    totalQuantityPadding: 36,
+    strokeWidth: 10,
+  },
+  xl: {
+    width: 600,
+    height: 700,
+    innerRadius: 150,
+    fontSizeTitle: "text-6xl",
+    fontSizeDescription: "text-2xl",
+    fontSizeSubTitle: "text-2xl",
+    fontSizeSubDescription: "text-xl",
+    fontSizeTotalTitle: "text-5xl",
+    fontSizeTotalDescription: "text-3xl",
+    totalQuantityPadding: 45,
+    strokeWidth: 10,
+  },
+} as const;
+
+export function PieChartDonutWithText({
+  chartData,
+  chartConfig,
+  dataKey,
+  nameKey,
+  title,
+  description,
+  subTitle,
+  subDescription,
+  isFinancial = false,
+  currency = "€",
+  size = "md",
+  customSize,
+}: PieChartDonutWithTextProps) {
+  const totalQuantity = React.useMemo(() => {
+    return chartData?.reduce((acc, curr) => acc + (curr[dataKey] || 0), 0) || 0;
+  }, [chartData, dataKey]);
+
+  const capitalizeString = (str: string) =>
+    str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+
+  const chartSize = customSize ?? CHART_SIZES[size];
+
+  return (
+    <Card
+      className={`flex flex-col min-w-[${chartSize.width}px] max-w-[${chartSize.width}px]]`}
+    >
+      <CardHeader className="items-center pb-0">
+        <CardTitle className={`${chartSize.fontSizeTitle}`}>{title}</CardTitle>
+        <CardDescription className={`${chartSize.fontSizeDescription}`}>
+          {description}
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer
+          config={chartConfig}
+          className="mx-auto aspect-square w-full"
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Pie
+              data={chartData}
+              dataKey={dataKey}
+              nameKey={nameKey}
+              innerRadius={chartSize.innerRadius}
+              strokeWidth={chartSize.strokeWidth}
+            >
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                    return (
+                      <text
+                        x={viewBox.cx}
+                        y={viewBox.cy}
+                        textAnchor="middle"
+                        dominantBaseline="middle"
+                      >
+                        <tspan
+                          x={viewBox.cx}
+                          y={viewBox.cy}
+                          className={`fill-foreground ${chartSize.fontSizeTotalTitle} font-bold`}
+                        >
+                          {(isFinancial ? currency : "") +
+                            totalQuantity.toLocaleString()}
+                        </tspan>
+                        <tspan
+                          x={viewBox.cx}
+                          y={(viewBox.cy || 0) + chartSize.totalQuantityPadding}
+                          className={`fill-muted-foreground ${chartSize.fontSizeTotalDescription}`}
+                        >
+                          {capitalizeString(dataKey)}
+                        </tspan>
+                      </text>
+                    );
+                  }
+                }}
+              />
+            </Pie>
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col gap-2">
+        <div
+          className={`flex items-center gap-2 font-medium leading-none ${chartSize.fontSizeSubTitle}`}
+        >
+          <TrendingUp className="h-4 w-4" /> {subTitle}
+        </div>
+        <div
+          className={`leading-none text-muted-foreground ${chartSize.fontSizeSubDescription}`}
+        >
+          {subDescription}
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}

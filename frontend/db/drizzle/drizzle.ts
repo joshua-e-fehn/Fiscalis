a39041796c2381b397c413acc39a4175bdf2cad7
Joshua Fehn <joshua.fehn@tum.de>
2025-02-22 17:39:24 +0100

    Change DrizzleORM database adapter
    For some reason the postgres adapter, did not work as expected.
    As a workaround the Neon DB adapter was chosen.
    So far, it looks like it is working flawlessly.

--- frontend/db/drizzle/drizzle.ts
+++ frontend/db/drizzle/drizzle.ts
@@ -1,8 +1,18 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+// import { drizzle } from "drizzle-orm/postgres-js";
+// import postgres from "postgres";
+
+// const connectionString = process.env.DATABASE_URL!;
+
+// // Disable prefetch as it is not supported for "Transaction" pool mode
+// export const client = postgres(connectionString, { prepare: false });
+// export const db = drizzle(client);
+
+import { Pool } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
+
+import * as schema from "./schema";
 
 const connectionString = process.env.DATABASE_URL!;
 
-// Disable prefetch as it is not supported for "Transaction" pool mode
-export const client = postgres(connectionString, { prepare: false });
-export const db = drizzle(client);
+const pool = new Pool({ connectionString });
+export const db = drizzle(pool, { schema: schema, logger: true });
